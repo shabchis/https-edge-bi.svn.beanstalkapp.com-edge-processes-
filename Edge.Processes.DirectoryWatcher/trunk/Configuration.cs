24c6d07fbe57babb2f2de4dcaaf341108d24d09b
doronassayas <doronassayas@e195cdbf-7edd-4c9c-8fbd-c72cb1ffe33a>
2011-08-03 15:42:05 +0000

    Adding the directory watcher

---
+++ Edge.Processes.DirectoryWatcher/trunk/Configuration.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace Edge.Processes.DirectoryWatcher
+{
+	public class DirectoryWatcherConfiguration : ConfigurationSection
+	{
+		public const string SectionName = "edge.processes.directoryWatcher";
+
+		[ConfigurationProperty("", IsDefaultCollection = true, IsRequired = true)]
+		public DirectoryElementCollection Directories
+		{
+			get { return (DirectoryElementCollection) base[""]; }
+		}
+	}
+
+	public class DirectoryElementCollection : Edge.Core.Configuration.ConfigurationElementCollectionBase<DirectoryElement>
+	{
+		public override ConfigurationElementCollectionType CollectionType
+		{
+			get { return ConfigurationElementCollectionType.BasicMap; }
+		}
+
+		protected override ConfigurationElement CreateNewElement()
+		{
+			return new DirectoryElement();
+		}
+
+		protected override object GetElementKey(ConfigurationElement element)
+		{
+			return element.ElementInformation.LineNumber;
+		}
+
+		protected override string ElementName
+		{
+			get { return "Directory"; }
+		}
+	}
+
+
+	public class DirectoryElement : ConfigurationElement
+	{
+		[ConfigurationProperty("Path", IsRequired = true)]
+		public string Path
+		{
+			get { return (string)base["Path"]; }
+		}
+
+		[ConfigurationProperty("Filter")]
+		public string Filter
+		{
+			get { return (string)base["Filter"]; }
+		}
+
+		[ConfigurationProperty("ServiceName")]
+		public string ServiceName
+		{
+			get { return (string)base["ServiceName"]; }
+		}
+
+		[ConfigurationProperty("AccountID", DefaultValue=-1)]
+		public int AccountID
+		{
+			get { return (int)base["AccountID"]; }
+		}
+
+		[ConfigurationProperty("SchedulerUrl")]
+		public string SchedulerUrl
+		{
+			get { return (string)base["SchedulerUrl"]; }
+		}
+
+		[ConfigurationProperty("SchedulerConfiguration")]
+		public string SchedulerConfiguration
+		{
+			get { return (string)base["SchedulerConfiguration"]; }
+		}
+
+		[ConfigurationProperty("IncludeSubdirs")]
+		public bool IncludeSubdirs
+		{
+			get { return (bool)base["IncludeSubdirs"]; }
+		}
+
+		// Support for options
+		public readonly Dictionary<string, string> ServiceOptions = new Dictionary<string, string>();
+		protected override bool OnDeserializeUnrecognizedAttribute(string name, string value)
+		{
+			this.ServiceOptions[name] = value;
+			return true;
+		}
+
+	}
+}
---
+++ Edge.Processes.DirectoryWatcher/trunk/DirectoryWatcher.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using Edge.Core;
+using Edge.Core.Scheduling;
+using Edge.Core.Services;
+using Edge.Core.Utilities;
+
+namespace Edge.Processes.DirectoryWatcher
+{
+	partial class DirectoryWatcher : ServiceBase
+	{
+		public const string FilePath = "FilePath";
+		public const string LogSource = "DirectoryWatcher";
+		Dictionary<FileSystemWatcher, DirectoryWatchInfo> _watchers = new Dictionary<FileSystemWatcher, DirectoryWatchInfo>();
+
+		public DirectoryWatcher()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			var configuration = (DirectoryWatcherConfiguration)ConfigurationManager.GetSection(DirectoryWatcherConfiguration.SectionName);
+
+			foreach (DirectoryElement dir in configuration.Directories)
+			{
+				if (String.IsNullOrWhiteSpace(dir.Path) || !Directory.Exists(dir.Path))
+					throw new ConfigurationErrorsException("Invalid path specified.",
+						dir.CurrentConfiguration!= null ?
+							dir.CurrentConfiguration.FilePath : string.Empty,
+						dir.ElementInformation.LineNumber);
+
+				if (String.IsNullOrWhiteSpace(dir.ServiceName))
+					throw new ConfigurationErrorsException("Invalid service name specified.",
+							dir.CurrentConfiguration != null ?
+							dir.CurrentConfiguration.FilePath : string.Empty,
+						dir.ElementInformation.LineNumber);
+
+				FileSystemWatcher watcher = new FileSystemWatcher(dir.Path);
+				watcher.NotifyFilter = NotifyFilters.FileName;
+				watcher.IncludeSubdirectories = dir.IncludeSubdirs;
+				if (!String.IsNullOrEmpty(dir.Filter))
+					watcher.Filter = dir.Filter;
+
+				watcher.Changed += new FileSystemEventHandler(watcher_Changed);
+				watcher.Created += new FileSystemEventHandler(watcher_Changed);
+
+				_watchers.Add(watcher, new DirectoryWatchInfo() { Config = dir, Watcher = watcher });
+			}
+
+			OnContinue();
+		}
+
+		protected override void OnContinue()
+		{
+			foreach (FileSystemWatcher watcher in _watchers.Keys)
+				watcher.EnableRaisingEvents = true;
+		}
+
+		protected override void OnPause()
+		{
+			foreach (FileSystemWatcher watcher in _watchers.Keys)
+				watcher.EnableRaisingEvents = false;
+		}
+
+		protected override void OnStop()
+		{
+			foreach (FileSystemWatcher watcher in _watchers.Keys)
+				watcher.Dispose();
+		}
+
+		void watcher_Changed(object sender, FileSystemEventArgs e)
+		{
+			Log.Write(LogSource, String.Format("{0} has been {1} ", e.Name, e.ChangeType), LogMessageType.Information);
+
+			FileSystemWatcher watcher = (FileSystemWatcher)sender;
+			DirectoryWatchInfo info;
+			if (!_watchers.TryGetValue(watcher, out info))
+			{
+				Log.Write(String.Format("No watcher found for {0} - check your configuration.", e.Name), LogMessageType.Warning);
+				return;
+			}
+
+			ServiceClient<IScheduleManager> scheduler;
+			
+			try
+			{
+				scheduler =
+					!String.IsNullOrWhiteSpace(info.Config.SchedulerUrl) || !String.IsNullOrWhiteSpace(info.Config.SchedulerConfiguration) ?
+						new ServiceClient<IScheduleManager>(info.Config.SchedulerConfiguration, info.Config.SchedulerUrl) :
+						new ServiceClient<IScheduleManager>();
+			}
+			catch (Exception ex)
+			{
+				Log.Write(String.Format(
+					"Could not create a connection to the scheduler{0}.",
+					String.IsNullOrWhiteSpace(info.Config.SchedulerUrl) ? string.Empty : " at " + info.Config.SchedulerUrl
+				), ex);
+				return;
+			}
+
+			// Make the request to the schedule manager
+			using (scheduler)
+			{
+				var options = new SettingsCollection();
+
+				try
+				{
+					foreach (var option in info.Config.ServiceOptions)
+						options.Add(option.Key, option.Value.Replace("{" + DirectoryWatcher.FilePath + "}", e.FullPath));
+				}
+				catch (Exception ex)
+				{
+					Log.Write("Error creating options for the service from the <Directory> configuration.", ex);
+				}
+
+				try
+				{
+					scheduler.Service.AddToSchedule(info.Config.ServiceName, info.Config.AccountID, DateTime.Now, options);
+				}
+				catch (Exception ex)
+				{
+					Log.Write(String.Format(
+						"Error trying to call AddToSchedule on the scheduler{0}.",
+						String.IsNullOrWhiteSpace(info.Config.SchedulerUrl) ? string.Empty : " at " + info.Config.SchedulerUrl
+					), ex);
+
+				}
+			}
+		}
+
+	}
+
+	public class DirectoryWatchInfo
+	{
+		public DirectoryElement Config;
+		public FileSystemWatcher Watcher;
+	}
+}
