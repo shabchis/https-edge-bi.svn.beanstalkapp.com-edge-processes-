f78699e7225866e8981b789642947ee84517d7ce
doronassayas <doronassayas@e195cdbf-7edd-4c9c-8fbd-c72cb1ffe33a>
2011-08-30 13:44:56 +0000

    Fixed process description.

--- Edge.Processes.DirectoryWatcher/trunk/Properties/AssemblyInfo.cs
+++ Edge.Processes.DirectoryWatcher/trunk/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Edge.Apps.FileSystemWatcher")]
+[assembly: AssemblyTitle("Edge Directory Watcher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Edge.Apps.FileSystemWatcher")]
+[assembly: AssemblyCompany("Edge.BI")]
+[assembly: AssemblyProduct("Edge Directory Watcher")]
 [assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
